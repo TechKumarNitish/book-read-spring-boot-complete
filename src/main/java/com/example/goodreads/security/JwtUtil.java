72588d0fccccf41277980ed497a7a95c986e8f88
TechKumarNitish <rskban15a2015@gmail.com>
2025-08-11 01:35:28 +0530

    added authz

--- src/main/java/com/example/goodreads/security/JwtUtil.java
+++ src/main/java/com/example/goodreads/security/JwtUtil.java
@@ -6,16 +6,17 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
     private final String SECRET = "replace_with_a_long_secure_secret_key_here";
     private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
-    public String generateToken(String username) {
+    public String generateToken(String username, String role) {
         return Jwts.builder()
                 .setSubject(username)
-                .addClaims(Map.of("roles", roles))
+                .addClaims(Map.of("role", role))
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1h
                 .signWith(key, SignatureAlgorithm.HS256)
@@ -31,13 +32,13 @@ public class JwtUtil {
                 .getSubject();
     }
 
-    public List<String> extractRoles(String token) {
+    public String extractRoles(String token) {
     Claims claims = Jwts.parserBuilder()
             .setSigningKey(key)
             .build()
             .parseClaimsJws(token)
             .getBody();
-    return claims.get("roles", List.class);
+    return claims.get("role", String.class);
 }
 
 }
